dcde6f157178e5d4273af093ad5bd435be8fae9a
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-23 22:56:08 +0200

    added test for constructor with size lesser than 1

--- SharpSnake/SharpSnake.Tests/Logic/BoardTests.cs
+++ SharpSnake/SharpSnake.Tests/Logic/BoardTests.cs
@@ -17,5 +17,21 @@ namespace SharpSnake.Tests.Logic
             Assert.AreEqual(25, board.Size.Area);
         }
 
+        [Test]
+        public void ConstructorWithSizeLesserThan1()
+        {
+            try
+            {
+                var size = new Vector(0, 1);
+                var board = new Board(size);
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual("Board size can not be lesser than one in any dimension.", e.Message);
+                return;
+            }
+
+            Assert.Fail("Exception not occured");
+        }
     }
 }
