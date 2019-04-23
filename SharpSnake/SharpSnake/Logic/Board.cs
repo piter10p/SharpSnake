4f16fc10b4c7da7b1fe1b3287da9b26430c5bade
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-23 22:42:18 +0200

    added constructor

--- SharpSnake/SharpSnake/Logic/Board.cs
+++ SharpSnake/SharpSnake/Logic/Board.cs
@@ -9,5 +9,14 @@ namespace SharpSnake.Logic
         private Field[] fieldsArray;
         
         public Vector Size { get; private set; }
+
+        public Board(Vector size)
+        {
+            if (size.X < 1 || size.Y < 1)
+                throw new Exception("Board size can not be lesser than one in any dimension.");
+
+            Size = size;
+            fieldsArray = new Field[size.Area];
+        }
     }
 }
