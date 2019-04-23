c61b7dacebcf73c431d98e2edcdddafa34a51d15
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-23 21:42:15 +0200

    created SharpSnake solution and project

---
+++ SharpSnake/SharpSnake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
