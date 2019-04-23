0c09a920f44b6ba6748bcce3f239c4c6b2820d3a
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-23 22:28:37 +0200

    added test for Equals method

--- SharpSnake/SharpSnake.Tests/Logic/VectorTests.cs
+++ SharpSnake/SharpSnake.Tests/Logic/VectorTests.cs
@@ -9,6 +9,16 @@ namespace SharpSnake.Tests.Logic
     [TestFixture]
     class VectorTests
     {
-        
+        [TestCase(0, 0, 0, 0, ExpectedResult = true)]
+        [TestCase(-35, -300000, -35, -300000, ExpectedResult = true)]
+        [TestCase(35, 300000, 35, 300000, ExpectedResult = true)]
+        [TestCase(0, 0, 1, 0, ExpectedResult = false)]
+        public bool Equals(int ax, int ay, int bx, int by)
+        {
+            var a = new Vector(ax, ay);
+            var b = new Vector(bx, by);
+
+            return a.Equals(b);
+        }
     }
 }
