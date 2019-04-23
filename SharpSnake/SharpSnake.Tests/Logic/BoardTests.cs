13d0f3c62ec6a326bac45ee44aa6f1f61e6b51dc
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-23 22:55:44 +0200

    added constructor test

--- SharpSnake/SharpSnake.Tests/Logic/BoardTests.cs
+++ SharpSnake/SharpSnake.Tests/Logic/BoardTests.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using SharpSnake.Logic;
 
 namespace SharpSnake.Tests.Logic
 {
     [TestFixture]
     class BoardTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var size = new Vector(5, 5);
+            var board = new Board(size);
+            Assert.AreEqual(25, board.Size.Area);
+        }
 
     }
 }
