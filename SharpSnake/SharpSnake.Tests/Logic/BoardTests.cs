b3efae497a8c9d367c6d876d2cd192a40459b962
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-23 22:44:34 +0200

    created BoardTests class

---
+++ SharpSnake/SharpSnake.Tests/Logic/BoardTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SharpSnake.Tests.Logic
+{
+    [TestFixture]
+    class BoardTests
+    {
+
+    }
+}
