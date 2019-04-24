36d2171603c4315f8af823bf6229812f08c0120b
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-24 15:34:54 +0200

    Add ReadInput static method to InputReader.

--- SharpSnake/SharpSnake/Logic/InputReader.cs
+++ SharpSnake/SharpSnake/Logic/InputReader.cs
@@ -6,5 +6,17 @@ namespace SharpSnake.Logic
 {
     public static class InputReader
     {
+        public static Input ReadInput()
+        {
+            var input = Input.None;
+
+            while(Console.KeyAvailable)
+            {
+                var key = Console.ReadKey();
+                input = GetInputFromKey(key);
+            }
+
+            return input;
+        }
     }
 }
