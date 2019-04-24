da832d4b3e0defbea1481ecd211edaa426ba46c5
Piotr Paszko <piotr.paszko.dev@gmail.com>
2019-04-24 15:35:13 +0200

    Add GetInputFromKey method to InputReader.

--- SharpSnake/SharpSnake/Logic/InputReader.cs
+++ SharpSnake/SharpSnake/Logic/InputReader.cs
@@ -18,5 +18,22 @@ namespace SharpSnake.Logic
 
             return input;
         }
+
+        private static Input GetInputFromKey(ConsoleKeyInfo key)
+        {
+            switch(key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    return Input.Up;
+                case ConsoleKey.DownArrow:
+                    return Input.Down;
+                case ConsoleKey.LeftArrow:
+                    return Input.Left;
+                case ConsoleKey.RightArrow:
+                    return Input.Left;
+            }
+
+            return Input.None;
+        }
     }
 }
